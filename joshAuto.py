710c4fe3e569585648ff6ad5084bf05c04a1cb40
Joshua <krjoshua21@gmail.com>
2022-12-20 13:31:02 +0900

    바탕화면에 저장, 원본파일 자동삭제 추가

--- joshAuto.py
+++ joshAuto.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import os
 
 rawair = pd.read_excel('C:/Users/woowahan/Downloads/여객_출발_시간표.xls')
 print("초기파일",rawair.shape)
@@ -12,17 +13,21 @@ rawair.drop(['운항편명','터미널','체크인 카운터','출발현황','CO
 print("불필요 컬럼 제거",rawair.shape)
 rawair.reset_index(drop=True, inplace=True)
 
+
 con1 = 11 < rawair['탑승구'] 
 con2 = rawair['탑승구'] < 25
 con3 = 29 < rawair['탑승구']
 con4 = rawair['탑승구'] < 42
-
 conair = rawair.loc[(con1 & con2) | (con3 & con4)]
 
 print("탑승구역 선별",conair.shape)
-print(type(conair))
 
+conair=conair.reset_index(drop=True)
+conair.index=conair.index+1
+conair.to_excel('C:/Users/woowahan/Desktop/딜리.xlsx')
 
-conair.to_excel('딜리.xlsx')
+print('바탕화면에 엑실파일 저장!')
 
-print('저장!')
+os.remove('C:/Users/woowahan/Downloads/여객_출발_시간표.xls')
+
+print('다운받은 원본 파일 삭제!')
